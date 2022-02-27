a013d9aee62cd543057770095039b5f135bab76b
valovski <vladi2005123@gmail.com>
2022-02-27 22:15:34 +0200

    Add UI

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+let playerScore = 0;
+let computerScore = 0;
+let result = "";
+
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -17,46 +21,119 @@ function playRound(playerSelection, computerSelection) {
   if (computerSelection == "Rock") {
     if (playerSelection.toLowerCase() == "rock") {
       console.log("Tie!");
+      result =
+        "Tie!" +
+        "\nPlayer score: " +
+        playerScore +
+        "\nComputer score: " +
+        computerScore;
+      checkIfWon();
     }
 
     if (playerSelection.toLowerCase() == "scissors") {
       console.log("You lose! Rock beats Scissors!");
+      computerScore++;
+      result =
+        "You lose! Rock beats Scissors!" +
+        "\nPlayer score: " +
+        playerScore +
+        "\nComputer score: " +
+        computerScore;
+      checkIfWon();
     }
 
     if (playerSelection.toLowerCase() == "paper") {
       console.log("You win! Paper beats Scissors!");
+      playerScore++;
+      result =
+        "You win! Paper beats Scissors!" +
+        "\nPlayer score: " +
+        playerScore +
+        "\nComputer score: " +
+        computerScore;
+      checkIfWon();
     }
   }
 
   if (computerSelection == "Paper") {
     if (playerSelection.toLowerCase() == "rock") {
       console.log("You lose! Paper beats Rock!");
+      computerScore++;
+      result =
+        "You lose! Paper beats Rock!" +
+        "\nPlayer score: " +
+        playerScore +
+        "\nComputer score: " +
+        computerScore;
+      checkIfWon();
     }
 
     if (playerSelection.toLowerCase() == "paper") {
       console.log("Tie!");
+      result =
+        "Tie!" +
+        "\nPlayer score: " +
+        playerScore +
+        "\nComputer score: " +
+        computerScore;
+      checkIfWon();
     }
 
     if (playerSelection.toLowerCase() == "scissors") {
       console.log("You win! Scissors beats Paper!");
+      playerScore++;
+      result =
+        "You win! Scissors beats Paper!" +
+        "\nPlayer score: " +
+        playerScore +
+        "\nComputer score: " +
+        computerScore;
+      checkIfWon();
     }
   }
 
   if (computerSelection == "Scissors") {
     if (playerSelection.toLowerCase() == "rock") {
       console.log("You win! Rock beats Scissors!");
+      playerScore++;
+      result =
+        "You win! Rock beats Scissors!" +
+        "\nPlayer score: " +
+        playerScore +
+        "\nComputer score: " +
+        computerScore;
+      checkIfWon();
     }
 
     if (playerSelection.toLowerCase() == "paper") {
       console.log("You lose! Scissors beats Paper!");
+      computerScore++;
+      result =
+        "You lose! Scissors beats Paper!" +
+        "\nPlayer score: " +
+        playerScore +
+        "\nComputer score: " +
+        computerScore;
+      checkIfWon();
     }
 
     if (playerSelection.toLowerCase() == "scissors") {
       console.log("Tie!");
+      result =
+        "Tie!" +
+        "\nPlayer score: " +
+        playerScore +
+        "\nComputer score: " +
+        computerScore;
+      checkIfWon();
     }
   }
+
+  document.querySelector(".results").innerText = result;
+  return;
 }
 
+/*
 function game() {
   for (let i = 0; i < 5; i++) {
     playRound(prompt("Rock, Paper, Scissors"), computerPlay());
@@ -65,3 +142,37 @@ function game() {
 }
 
 game();
+*/
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+
+rock.addEventListener("click", () => {
+  playRound("Rock", computerPlay());
+});
+
+paper.addEventListener("click", () => {
+  playRound("Paper", computerPlay());
+});
+
+scissors.addEventListener("click", () => {
+  playRound("Scissors", computerPlay());
+});
+
+const results = document.querySelector(".results");
+
+function checkIfWon() {
+  if (playerScore == 5) {
+    result += "\nI won!";
+    document.querySelector(".rock").disabled = true;
+    document.querySelector(".paper").disabled = true;
+    document.querySelector(".scissors").disabled = true;
+  }
+
+  if (computerScore == 5) {
+    result += "\nI lost!";
+    document.querySelector(".rock").disabled = true;
+    document.querySelector(".paper").disabled = true;
+    document.querySelector(".scissors").disabled = true;
+  }
+}
